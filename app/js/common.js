0d168f2c50cfade6bd09b2fb66160d568d22667c
mavregg <wild_power@list.ru>
2018-04-05 20:38:38 +0300

    Section 3 compleated

--- app/js/common.js
+++ app/js/common.js
@@ -6,13 +6,19 @@ $(document).ready(function () {
 
     $(".content-wrapper__name, .content-wrapper__dscr").animated("fadeInDown", "fadeOutUp");
 
-    $(".section__wrapper").animated("fadeInDown", "fadeOutUp");
-
     $(".popup").magnificPopup({type : "image"});
+    $(".popup_content").magnificPopup({type:'inline',
+        midClick: true});
 
     $(".section_animation-1").animated("fadeInLeft", "fadeOutLeft");
     $(".section_animation-2").animated("fadeInDown", "fadeOutUp");
     $(".section_animation-3").animated("fadeInRight", "fadeOutRight");
+    $(".section_animation-4").animated("fadeInUp", "fadeOutDown");
+
+    $(".section-portfolio-item").each(function(i) {
+        $(this).find("a").attr("href", "#work" + i);
+        $(this).find(".section-portfolio-item-decr").attr("id", "work" + i);
+    });
 
 
 
