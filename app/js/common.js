8ba8aba0888afcf055cdc1a23c5bc9932ee41b9a
mavregg <wild_power@list.ru>
2018-04-06 14:19:04 +0300

    fixes

--- app/js/common.js
+++ app/js/common.js
@@ -4,12 +4,6 @@ $(document).ready(function () {
         $(".menu").slideToggle();
     });
 
-    $(".content-wrapper__name, .content-wrapper__dscr").animated("fadeInDown", "fadeOutUp");
-
-    $(".popup").magnificPopup({type : "image"});
-    $(".popup_content").magnificPopup({type:'inline',
-        midClick: true});
-
     $(".section_animation-1").animated("fadeInLeft", "fadeOutLeft");
     $(".section_animation-2").animated("fadeInDown", "fadeOutUp");
     $(".section_animation-3").animated("fadeInRight", "fadeOutRight");
@@ -20,7 +14,15 @@ $(document).ready(function () {
         $(this).find(".section-portfolio-item-decr").attr("id", "work" + i);
     });
 
-
+    // $(".menu-wrapper ul a").mPageScroll2id();
+    $("a.scroll_2").click(function() {
+        $.scrollTo($(".section_bgc_e7"), 800, {
+            offset: 100
+        });
+    });
+    $("a.scroll_2").scrollTo($(".section_bgc_e7"));
+// <li class="nav_item"><a href="#pointer1" class="scroll_2" onclick="currentItem(1)">Выбрать пиццу</a></li>
+//     <div id="pointer1" class="pointer pointer_1"><i class="fa fa-angle-double-down" aria-hidden="true"></i></div>
 
 });
 $(window).load(function () {
