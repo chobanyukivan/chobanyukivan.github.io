110a24e4a8456f4bfc1c91678611f8c64e08bce3
mavregg <wild_power@list.ru>
2018-04-16 16:23:12 +0300

    dist added

--- gulpfile.js
+++ gulpfile.js
@@ -15,33 +15,29 @@ var gulp 		= require('gulp'),
 gulp.task("sass", function(){
 	return gulp.src("app/sass/**/*.sass")
 	.pipe(sass())
-	// .pipe(autoprefixer(['last 15 versions','>1%', 'ie 8'], {cascade:true}))
+	.pipe(autoprefixer({cascade:true}))
 	.pipe(gulp.dest("app/css"));
 });
 
-gulp.task('scripts', function(){
+gulp.task('script-Libs', function(){
 	return gulp.src(['app/libs/**/*.js'])
 	.pipe(concat('libs.min.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('dist/js'));
+	.pipe(gulp.dest('dist/libs'));
 });
 
-
-gulp.task('concss', function () {
- return gulp.src('app/css/**/*.css')
-   .pipe(concatCss("style.css"))
-   .pipe(gulp.dest('dist/css'));
-});
-
+// gulp.task('concss', function () {
+//  return gulp.src('app/css/**/*.css')
+//    .pipe(concatCss("styles.css"))
+//    .pipe(gulp.dest('dist/css'));
+// });
 
 gulp.task("css-libs", ['sass'], function() {
-	return gulp.src("app/**/*.css")
-	// 
-	.pipe(concatCss("style.css"))
-	// 
-	.pipe(cssnano("style.css"))
+	return gulp.src("app/libs/**/*.css")
+	.pipe(concatCss("libs.css"))
+	.pipe(cssnano("libs.css"))
 	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest("dist/css"));
+	.pipe(gulp.dest("dist/libs"));
 });
 
 gulp.task("browsersync", function(){
@@ -72,18 +68,22 @@ gulp.task("img", function() {
 	.pipe(gulp.dest('dist/img'))
 });
 
-gulp.task("watch", ['browsersync','css-libs', 'scripts'], function(){
+// gulp.task("watch", ['browsersync','css-libs', 'script-Libs'], function(){
+gulp.task("watch", ['browsersync'], function(){
 	gulp.watch("app/sass/**/*.sass",["sass"], browsersync.reload);
 	gulp.watch("app/css/**/*.css", browsersync.reload);
 	gulp.watch("app/*.html", browsersync.reload);
-	gulp.watch("app/js/**/*.js", browsersync.reload);
+	gulp.watch("app/js/*.js", browsersync.reload);
 });
 
-gulp.task('build', ['clean','img','sass','scripts', "css-libs"], function() {
+gulp.task('build', ['clean','img','sass','script-Libs', "css-libs"], function() {
 	
-	// var buildCss = gulp.src([
-	// 	'app/css/*.css'])
-	// .pipe(gulp.dest('dist/css'));
+	var buildCss = gulp.src([
+		'app/css/*.css'])
+		.pipe(concatCss("styles.css"))
+        .pipe(cssnano("styles.css"))
+        .pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/css'));
 
 	var buildFonts = gulp.src('app/fonts/**/*')
 	.pipe(gulp.dest('dist/fonts'));
@@ -96,4 +96,7 @@ gulp.task('build', ['clean','img','sass','scripts', "css-libs"], function() {
 
 	var buildIco = gulp.src('app/*.png')
 	.pipe(gulp.dest('dist'));
+
+    var buildPDF = gulp.src('app/*.pdf')
+		.pipe(gulp.dest('dist'));
 });
